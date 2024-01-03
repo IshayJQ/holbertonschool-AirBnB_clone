99fe599e94acbd016fa337f73ecc357188243ec8
Ishay Junnco <6905@holbertonstudents.com>
2024-01-03 10:06:05 -0800

    modificate flie storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,15 +25,38 @@ class FileStorage:
     def reload(self):
         
         load_data = {}
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+            'BaseModel': BaseModel, 'Users': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
 
         try:
             with open(self.__file_path, mode='r') as file:
-                load_data = load(file)
+                load_data = json.load(file)
+                for key, kwargs in load_data.items():
+                    self.all()[key] = classes[kwargs['__class__']](**kwargs)
         except FileNotFoundError:
             pass
-
+        """
         for key, kwargs in load_data.items():
             class_name = kwargs["__class__"]
             self.__objects[key] = eval(class_name)(**kwargs)
+        """
 
+        def delete(self, obj=None):
+            if obj is not None:
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                self.__objects.pop(key)
+            self.save()
 
+        def close(self):
+            self.reload()
